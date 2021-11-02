77444fae3dd180c4831a4cb760b08d508048901f
Alejandro <eleutherococ@yandex.ru>
2021-11-02 12:33:39 +0700

    before create migrations

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+# from django.utils.translation import gettext_lazy as _
+
+
+class User(AbstractUser):
+    # email = models.EmailField(max_length=254, blank=False)
+
+
+    USER = 'USR'
+    MODERATOR = 'MDR'
+    ADMIN = 'ADM'
+    ROLE_CHOICES = [
+        (USER, 'user'),
+        (MODERATOR, 'moderator'),
+        (ADMIN, 'admin'),
+    ]
+    role = models.CharField(
+        max_length=3,
+        choices=ROLE_CHOICES,
+        default=USER,
+    )
+    bio = models.TextField('Биография', blank=True,)
+
+    REQUIRED_FIELDS = ['email']
