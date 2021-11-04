b1bd1526d49341be244146e3905e02a2cc33efb6
stas-zatushevskii <86684411+stas-zatushevskii@users.noreply.github.com>
2021-11-04 20:08:29 +0300

    a

--- api_yamdb/api_yamdb/api/views.py
+++ api_yamdb/api_yamdb/api/views.py
@@ -14,28 +14,3 @@ class AdminViewSet(viewsets.ModelViewSet):
 class UserViewSet(viewsets.ModelViewSet):
     pass
 
-# вьюсеты v1
-class ReviewViewSet(viewsets.ModelViewSet):
-    serializer_class = ReviewSerializer
-
-    def get_queryset(self):
-        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
-        new_queryset = title.reviews.all()
-        return new_queryset
-
-    def perform_create(self, serializer):
-        get_object_or_404(Title, id=self.kwargs.get('title_id'))
-        serializer.save(user=self.request.user)
-
-
-class CommentViewSet(viewsets.ModelViewSet):
-    serializer_class = CommentSerializer
-    
-    def get_queryset(self):
-        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
-        new_queryset = title.comments.all()
-        return new_queryset
-
-    def perform_create(self, serializer):
-        get_object_or_404(Title, id=self.kwargs.get('title_id'))
-        serializer.save(author=self.request.user)
