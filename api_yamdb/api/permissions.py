34b9f2d048c73e8c6492c7a47865795c0fd8fedb
stas-zatushevskii <86684411+stas-zatushevskii@users.noreply.github.com>
2021-11-20 23:36:48 +0300

    s

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -12,9 +12,21 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
 
 class IsAuthenticatedOrOwnerOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
+    def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated and obj.author == request.user or request.user.is_admin)
+                or request.user.is_authenticated
+                    )
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_anonymous:
+            return True
+        else:
+            return (
+                obj.author == request.user
+                or request.user.role == 'admin'
+                or request.user.role == 'moderator'
+                or request.user.is_staff
+            )
 
 
 class IsAdmin(BasePermission):
