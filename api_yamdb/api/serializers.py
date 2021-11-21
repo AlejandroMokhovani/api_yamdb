5a1e171a83c98f209bb1702a074ca868a4830135
stas-zatushevskii <86684411+stas-zatushevskii@users.noreply.github.com>
2021-11-22 01:01:35 +0300

    try/except

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -5,6 +5,8 @@ from reviews.models import Category, Comment, Genre, Review, Title, User
 from django.core.exceptions import ValidationError
 from django.shortcuts import get_object_or_404
 
+
+
 class CreateUserSerializer(serializers.ModelSerializer):
 
     def validate_email(self, value):
