74dfae90fa92aaf02d124d6ad6aec416a7434fd2
Alexander Avdeenko <dvaxela@yandex.ru>
2021-11-23 19:40:47 +0300

    все правки внесены

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -23,12 +23,11 @@ class UserPermission(permissions.BasePermission):
                 or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-        if request.user.is_anonymous:
+        if request.method in permissions.SAFE_METHODS:
             return True
-        else:
-            return (
-                obj.author == request.user
-                or request.user.role == 'admin'
-                or request.user.role == 'moderator'
-                or request.user.is_staff
-            )
+        return (
+            obj.author == request.user
+            or request.user.role == 'admin'
+            or request.user.role == 'moderator'
+            or request.user.is_staff
+        )
