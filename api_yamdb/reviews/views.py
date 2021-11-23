fdccdf334d67b000189b21ec190ed771aa727e0b
Alexander Avdeenko <dvaxela@yandex.ru>
2021-11-23 19:31:14 +0300

    Исправлена вторая ошибка во вьюхе

--- api_yamdb/reviews/views.py
+++ api_yamdb/reviews/views.py
@@ -79,10 +79,14 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = (UserPermission,)
 
     def get_queryset(self):
-        review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
+        review = get_object_or_404(Review,
+                                   title_id=self.kwargs.get('title_id'),
+                                   id=self.kwargs.get('review_id'))
         new_queryset = review.comments.all()
         return new_queryset
 
     def perform_create(self, serializer):
-        review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
+        review = get_object_or_404(Review,
+                                   title_id=self.kwargs.get('title_id'),
+                                   id=self.kwargs.get('review_id'))
         serializer.save(author=self.request.user, reviews=review)
